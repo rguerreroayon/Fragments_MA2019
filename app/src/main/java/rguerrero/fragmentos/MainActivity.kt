0372a5f754cd87fec8ac66b55da487e4e6a2e611
unknown <LV1823@LV1823-09.itson.edu.mx>
2019-03-11 13:01:53 -0700

    Listo

--- app/src/main/java/rguerrero/fragmentos/MainActivity.kt
+++ app/src/main/java/rguerrero/fragmentos/MainActivity.kt
@@ -2,11 +2,24 @@ package rguerrero.fragmentos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonCambio.setOnClickListener{
+            val transaccion = supportFragmentManager.beginTransaction()
+
+            val fragmentoUno = PrimerFragmento()
+
+            transaccion.replace(R.id.frameCambio,fragmentoUno)
+
+            transaccion.addToBackStack(null)
+            transaccion.commit()
+
+        }
     }
 }
